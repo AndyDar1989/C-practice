57dbc51d39938e155e002c83134f65b1c0fff07a
AndrewDar <geologalone@gmail.com>
2022-08-03 19:02:20 +0300

    dog run code

--- example03/Program.cs
+++ example03/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int friend;
+int time;
+int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+friend = 2;
+while (distance>10)
+{
+    if (friend == 1)
+    {time = distance/(firstFriendSpeed+dogSpeed);
+    friend = 2;} 
+    else
+    {time = distance/(secondFriendSpeed+dogSpeed);
+    friend = 1;} 
+distance = distance - (firstFriendSpeed+secondFriendSpeed)*time;
+count++;}
+Console.Write("Dog will run ");
+Console.Write(count);
+Console.Write(" times");
